d60049580efb937a735b924f8ab4219422cc20b4
Baptiste LEGO <blego@lithium.paris>
2022-01-27 20:14:15 +0100

    add deploy contracts

--- contract/migrations/2_deploy_contracts.js
+++ contract/migrations/2_deploy_contracts.js
@@ -1,5 +1,9 @@
 const VoteFactory = artifacts.require('VoteFactory');
+const CandidateFactory = artifacts.require('CandidateFactory');
+const ElectionFactory = artifacts.require('ElectionFactory');
 
 module.exports = (deployer) => {
   deployer.deploy(VoteFactory);
+  deployer.deploy(CandidateFactory);
+  deployer.deploy(ElectionFactory);
 };
